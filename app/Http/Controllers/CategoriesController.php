776058c706fddc339502001e596fbb8ddf41c85c
Michel Lima <michelpl@yahoo.com.br>
2016-09-29 21:11:32 -0300

    Custom Request.

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,18 +3,32 @@
 namespace CodeDelivery\Http\Controllers;
 
 use CodeDelivery\Repositories\CategoryRepository;
+use Illuminate\Http\Request;
+use CodeDelivery\Http\Requests\AdminCategoryRequest;
 
 use CodeDelivery\Http\Controllers\Controller;
 
 class CategoriesController extends Controller
 {
-    public function index(CategoryRepository $repository) {
-        $categories = $repository->paginate(5);
+    private $repository;
+    
+    public function __construct(CategoryRepository $repository) {
+        $this->repository = $repository;
+    }
+    
+    public function index() {
+        $categories = $this->repository->paginate();
         return view("admin.categories.index", compact('categories'));
     }
     
-    public function create(CategoryRepository $repository) {
-        $categories = $repository->paginate(5);
-        return view("admin.categories.create", compact('categories'));
+    public function create() {
+        return view("admin.categories.create");
+    }
+    public function store(AdminCategoryRequest $request) {
+        
+        $data = $request->all();
+        $this->repository->create($data);
+        return redirect()->route("admin.categories.index");
+                
     }
 }
--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -4,11 +4,24 @@
     <div class="container">
         <h3>Nova categoria</h3>
         
+            @if($errors->any())
+            <ul class="alert">
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+            @endif
+            {!! Form::open(['route' => 'admin.categories.store']) !!}
+            <div class="form-group">
+                    {!! Form::label('name', 'Nome')  !!}
+                    {!! Form::text('name',null, ['class' => 'form-control'])  !!}
+                    
+            </div>
+            <div class="form-group">
+                    {!! Form::submit('Salvar', ['class' => 'btn btn-primary', 'value' => 'Salvar'])  !!}
+            </div>
+            {!! Form::close() !!}
         
-        <p><a href="#" class="btn btn-default">Nova categoria</a></p>
-        <br>
-        
-        {!! $categories->render() !!}
     </div>
     
 
