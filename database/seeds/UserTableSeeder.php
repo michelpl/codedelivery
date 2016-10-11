936b847caa6a46cc530c637cdfdf197c5fa5d5c6
Michel Lima <michelpl@yahoo.com.br>
2016-10-11 16:10:43 -0300

    Validando role.

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,6 +12,22 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
+        factory(User::class)->create([
+            'name' => 'User',
+            'email' => 'user@user.com',
+            'password' => bcrypt(123456),
+            'remember_token' => str_random(10)
+        ]);
+
+        factory(User::class)->create([
+            'name' => 'Admin',
+            'email' => 'admin@user.com',
+            'password' => bcrypt(123456),
+            'remember_token' => str_random(10),
+            'role' => 'admin'
+        ]);
+
+
         factory(User::Class, 10)->create()->each(function($u){
             $u->client()->save(factory(Client::class)->make());
         });
